51b96e85daa4200e6b0fef969901baf93b8d9d9a
John <johndarke@live.co.uk>
2020-06-08 15:37:02 +0100

    path change

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-app.use(favicon('/build/favicon.ico'));
+app.use(favicon('build/favicon.ico'));
 // the / is the current directory from where the script is running
 app.use(express.static('/'));
-app.use(express.static(path.join('/', 'build')));
+app.use(express.static(path.join('', 'build')));
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
 app.get('/*', function (req, res) {
-  res.sendFile(path.join('/', 'build', 'index.html'));
+  res.sendFile(path.join('', 'build', 'index.html'));
 });
 app.listen(port)
