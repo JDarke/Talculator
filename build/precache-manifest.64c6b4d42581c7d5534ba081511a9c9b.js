ee7ae2c7de95740a468f0ad4ef2e565afc209af0
John <johndarke@live.co.uk>
2020-06-13 12:32:07 +0100

    herokubuild

---
+++ build/precache-manifest.64c6b4d42581c7d5534ba081511a9c9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "282c4b5a93a64cb00609a6e027ed669e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "358fa6b44ec152e8b585",
+    "url": "/static/css/main.bf141b7a.chunk.css"
+  },
+  {
+    "revision": "3e6bc5290ffad69bd30f",
+    "url": "/static/js/2.cd3486bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cd3486bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "358fa6b44ec152e8b585",
+    "url": "/static/js/main.c985f662.chunk.js"
+  },
+  {
+    "revision": "b85ded5ed4341b549adb",
+    "url": "/static/js/runtime-main.f3098e27.js"
+  }
+]);
