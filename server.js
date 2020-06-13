9b2d6ce57c193a80d4b638823954843971af405d
John <johndarke@live.co.uk>
2020-06-13 12:58:51 +0100

    server tweak

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '..', 'public');
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
+    res.sendFile('./public/index.html');
   });
   
 app.listen(port, () => {
